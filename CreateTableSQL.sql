2f916a5edd0a507a9da256abeeb20f075aa9accf
Lorenzo Tasca <tasca.lorenzo.studente@itispaleocapa.it>
2023-12-21 11:40:13 +0000

    HoToltoVirgole

--- CreateTableSQL.sql
+++ CreateTableSQL.sql
@@ -7,35 +7,35 @@ CREATE TABLE Giocatore(
     standing_reach integer NOT NULL,
     altezza_senza_scarpe integer NOT NULL,
     apertura_alare integer NOT NULL,
-    PRIMARY KEY (ID_giocatore),
-)
+    PRIMARY KEY (ID_giocatore)
+);
 
 CREATE TABLE Scheda(
     ID_scheda char(10),
     giorni_della_settimana char(7),
     orario integer,
-    PRIMARY KEY (ID_scheda),
-)
+    PRIMARY KEY (ID_scheda)
+);
 
 CREATE TABLE Allenamento(
     ID_allenamento char(10),
     orario integer,
     descrizione varchar(1000),
-    PRIMARY KEY (ID_allenamento),
-)
+    PRIMARY KEY (ID_allenamento)
+);
 
 CREATE TABLE Statistica(
-    ID_statistica char(20) PRIMARY KEY,
-)
+    ID_statistica char(20) PRIMARY KEY
+);
 
 CREATE TABLE comprende(
     ID_allenamento char(10),
     ID_scheda char(10),
-    PRIMARY KEY (ID_allenamento, ID_scheda),
-)
+    PRIMARY KEY (ID_allenamento, ID_scheda)
+);
 
 CREATE TABLE riceve(
     ID_giocatore char(10),
     ID_scheda char(10),
-    PRIMARY KEY (ID_giocatore, ID_scheda),
-)
+    PRIMARY KEY (ID_giocatore, ID_scheda)
+);
