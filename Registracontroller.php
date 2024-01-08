cbe616ed132658624487bf198c28cb3706c9bbfb
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-01-08 16:17:55 +0000

    modified:   Registracontroller.php

--- Registracontroller.php
+++ Registracontroller.php
@@ -1,33 +1,33 @@
 <?php
 
-var_dump($_POST);
-$name = $_POST['name'];
-$username = $_POST['username'];
-$password = $_POST['password'];
+    var_dump($_POST);
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$password = md5($password);
+    $password = md5($password);
 
-$connessione = new mysqli('localhost', 'root', '', 'progetto');
+    $connessione = new mysqli('localhost', 'root', '', 'progetto');
 
-if ($connessione->connect_errno) {
-    echo('Connessione fallita: ' . $connessione->connect_error);
-    exit();
-}else{
-    try{
+    if ($connessione->connect_errno) {
+        echo('Connessione fallita: ' . $connessione->connect_error);
+        exit();
+    }else{
+        try{
 
-        $registra = "insert into user (name, username, password) values ('$name', '$username', '$password')";
-        $connessione->query($registra);
-        header("Location: Login.php?err=$err");  
-    }   
-    catch(Exception $e){
-        $err = $e->getMessage();
+            $registra = "insert into user (name, username, password) values ('$name', '$username', '$password')";
+            $connessione->query($registra);
+            header("Location: Login.php?err=$err");  
+        }   
+        catch(Exception $e){
+            $err = $e->getMessage();
 
-        //redirect su Registra.php
-        header("Location: Registra.php?err=$err");   
+            //redirect su Registra.php
+            header("Location: Registra.php?err=$err");   
 
+        }
+        
+        
     }
-    
-    
-}
 
 ?>
