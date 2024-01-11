b21e2a45d5a48be02df77c5f975c3612d1f9cc1a
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-01-11 09:30:34 +0000

    modified:   Registra.php

--- Registra.php
+++ Registra.php
@@ -4,8 +4,8 @@
 
     <body>
         <form action="Registra.php" method="post">
-            <input type="text" name="name" placeholder="Name" required><br><br><br>
-            <input type="text" name="username" placeholder="Username" required><br><br><br>
+            <input type="text" name="name" placeholder="Name" required><br><br>
+            <input type="text" name="username" placeholder="Username" required><br><br>
             <input type="text" name="password" placeholder="Password" required><br><br><br>
             <input type="submit" name="login" placeholder="Login" required><br><br><br>
         </form>
