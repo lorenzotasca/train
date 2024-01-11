06c3ef1b7f7afc5e380b55486e78464834e726f6
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-01-11 09:29:47 +0000

    aggiunti <br>

--- Registra.php
+++ Registra.php
@@ -4,10 +4,10 @@
 
     <body>
         <form action="Registra.php" method="post">
-            <input type="text" name="name" placeholder="Name" required><br>
-            <input type="text" name="username" placeholder="Username" required><br>
-            <input type="text" name="password" placeholder="Password" required><br>
-            <input type="submit" name="login" placeholder="Login" required><br>
+            <input type="text" name="name" placeholder="Name" required><br><br><br>
+            <input type="text" name="username" placeholder="Username" required><br><br><br>
+            <input type="text" name="password" placeholder="Password" required><br><br><br>
+            <input type="submit" name="login" placeholder="Login" required><br><br><br>
         </form>
         <?php
             if(isset($_GET['err'])){
