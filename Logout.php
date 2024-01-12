02a8e27fca674d9d819979c56c8e216f92cf1814
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-01-12 16:48:22 +0000

    modified:   Logout.php modified:   Profile.php

--- Logout.php
+++ Logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    $_SESSION['id'] = null;
-    $_SESSION['name'] = null;
+    $_SESSION['ID_giocatore'] = null;
+    $_SESSION['nome'] = null;
 
     session_destroy();
     
