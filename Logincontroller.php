83f64dc487b59edc26119df96f8d8288095d0c36
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-01-12 15:40:24 +0000

    modified:   Logincontroller.php

--- Logincontroller.php
+++ Logincontroller.php
@@ -30,17 +30,17 @@
 
         if($result->num_rows > 0){
             $row = $result->fetch_assoc();
-            if(password_verify($password, $row['password'])){
+            if(password_verify($password, $row['passwordCode'])){
                 // Password matches, so create the session
                 $_SESSION['id'] = $row['ID_giocatore'];
                 $_SESSION['username'] = $row['username'];
                 // Redirect to profile page
                 header("Location: Profile.php");
-                //exit(); // Importante per interrompere l'esecuzione dopo il redirect
-            } else {
+                exit(); // Importante per interrompere l'esecuzione dopo il redirect
+            }else {
                 echo "Incorrect username or password.";
             }
-        } else {
+        }else {
             echo "Incorrect username or password.";
         }
 
@@ -48,50 +48,5 @@
     }
 
     $connessione->close();
-    /*
-    // Start the session
-    session_start();
-
     
-    // Database configuration
-    $db_host = 'localhost';
-    $db_user = 'root';
-    $db_pass = '';
-    $db_name = 'progetto';
-
-    // Create a new MySQLi connection
-    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-    
-
-    $conn = new mysqli('localhost', 'root', '', 'progetto');
-
-    // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // Check if form is submitted
-    if(isset($_POST['submit'])){
-        // Sanitize user input to prevent SQL injection
-        $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $password = mysqli_real_escape_string($conn, $_POST['password']);
-
-        // Query the database for username and password
-        $result = mysqli_query($conn, "SELECT * FROM Giocatore WHERE username = '$username'");
-        if(mysqli_num_rows($result) > 0){
-            $row = mysqli_fetch_assoc($result);
-            if(password_verify($password, $row['password'])){
-                // Password matches, so create the session
-                $_SESSION['id'] = $row['ID_giocatore'];
-                $_SESSION['username'] = $row['username'];
-                // Redirect to profile page
-                header("Location: Profile.php");
-            }else{
-                echo "Incorrect username or password.";
-            }
-        }else{
-            echo "Incorrect username or password.";
-        }
-    }
-    */
 ?>
