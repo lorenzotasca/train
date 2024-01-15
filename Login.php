73280f642e110f3429879ed7a03da64b778f7904
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-01-15 11:42:40 +0000

    modified:   Login.php

--- Login.php
+++ Login.php
@@ -9,16 +9,17 @@
         <form action="Logincontroller.php" method="post">
             <input type="text" name="username" placeholder="Username" required><br><br>
             <input type="text" name="password" placeholder="Password" required><br><br><br>
-            <input type="submit" name="login" value="Submit">
-            <?php
-                if(isset($_POST['login'])){
-                    if(isset($_SESSION['error'])) {
-                        echo "<p>".$_SESSION['error']."</p>";
-                        //unset($_SESSION['error']); // Clear the error message
-                    }
-                }
-            ?>
+            <input type="submit" name="login" value="Submit" onClick="submitClick(e);">
+            
         </form>
+
+        <script>
+            function submitClick(e) {
+                e.preventDefault();
+                var t = document.getElementById("username");
+                t.innerHTML = "Hello World"; // mettere riga da 13 a 20 
+            }
+        </script>
         
         <a href="Registra.php">Don't have an account yet?</a>
     </body>
