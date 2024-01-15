2bbe0c6bb67cc28538671fd4673501ffe3d22b11
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-01-15 12:36:39 +0000

    modified:   Login.php

--- Login.php
+++ Login.php
@@ -12,14 +12,33 @@
             <input type="submit" id="login" name="login" value="Submit" onClick="submitClick(e);">
             
         </form>
-        <?php
+        <script>
+            <?php
                 if(isset($_POST['login'])){
                     if(isset($_SESSION['error'])) {
-                        echo "<span>".$_SESSION['error']."</span>";
-                        unset($_SESSION['error']); // Clear the error message
+                        //echo "<span>".$_SESSION['error']."</span>";
+                        //unset($_SESSION['error']); // Clear the error message
+            ?>
+                document.addEventListener("DOMContentLoaded", function(event) {
+                    console.log("DOM fully loaded and parsed");
+                    var botton = document.getElementById("login");
+                    document.getElementById("login").addEventListener("click", submitClick);
+                });
+
+                function submitClick(e) {
+                    e.preventDefault();
+                    var t = document.getElementById("login");
+                    //t.innerHTML = "Hello World"; 
+                    var message = document.createElement("span");// write the message in the same line of the button
+                    message.textContent = $_SESSION['error']; 
+                    t.parentNode.insertBefore(message, t.nextSibling);
+                }
+            <?php
                     }
                 }
-        ?>
+            ?>
+        </script>
+        
         
         
         <a href="Registra.php">Don't have an account yet?</a>
