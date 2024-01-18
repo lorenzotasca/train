bc13a29879348ba36ce199a0351f7fe204dcf814
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-01-18 09:35:00 +0000

    modified:   API.php

--- API.php
+++ API.php
@@ -13,7 +13,7 @@ $dbname = "progetto";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 //query
-$sql = "SELECT * FROM Giocatore WHERE nome LIKE '%$param%'";
+$sql = "SELECT * FROM Giocatore WHERE nome LIKE '%*$param*%'";
 
 //execute query
 $result = $conn->query($sql);
