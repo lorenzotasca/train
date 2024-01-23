24a4c4d9801975c5714967751d8b5853b8662324
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-01-23 18:10:39 +0000

    modified:   Registra.php

--- Registra.php
+++ Registra.php
@@ -23,8 +23,8 @@
             overflow: hidden;
             width: 400px;
             text-align: center;
-            margin-top: 80px; /* Aumentato il margine superiore */
-        }
+            margin-top: 120px;
+            margin-bottom: 80px;
 
         .register-container h2 {
             color: #333333;
