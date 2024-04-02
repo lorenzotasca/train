4934c88855a42c4c85b3b16cb18b3033731268ec
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-04-02 10:34:23 +0000

    modified:   Tab.php

--- Tab.php
+++ Tab.php
@@ -1,117 +1,112 @@
 <!DOCTYPE html>
 <html lang="it">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Programming</title>
-        <style>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programming</title>
+    <style>
+        table {
+            width: 94%;
+            border-collapse: collapse;
+            float: left;
+        }
 
-            table {
-                width: 94%;
-                border-collapse: collapse;
-                float: left;
-            }
+        table, th, td {
+            border: 1px solid black;
+            text-align: center;
+            padding: 10px;
+        }
 
-            table, th, td {
-                border: 1px solid black;
-                text-align: center;
-                padding: 10px;
-            }
+        th {
+            background-color: #f2f2f2;
+        }
 
-            th {
-                background-color: #f2f2f2;
-            }
-            
-            .legend {
-                margin-top: 20px;
-                float: left;
-                margin-right: 20px;
-            }
+        .legend {
+            margin-top: 20px;
+            float: left;
+            margin-right: 20px;
+        }
 
-            .legend span {
-                display: block;
-                margin-bottom: 10px;
-                width: 20px;
-                height: 20px;
-                border-radius: 50%;
-            }
+        .legend span {
+            display: block;
+            margin-bottom: 10px;
+            width: 20px;
+            height: 20px;
+            border-radius: 50%;
+        }
 
-            .Basket {
-                background-color: #ff9999;
-            }
+        .Basket {
+            background-color: #ff9999;
+        }
 
-            .Gym {
-                background-color: #99ccff;
-            }
+        .Gym {
+            background-color: #99ccff;
+        }
 
-            .Personal_growth {
-                background-color: #99ff99;
-            }
+        .Personal_growth {
+            background-color: #99ff99;
+        }
 
-            .Sleep {
-                background-color: #ffff99;
-            }
+        .Sleep {
+            background-color: #ffff99;
+        }
 
-        </style>
-    </head>
-    <body>
+        .Other {
+            background-color: #ffcc99;
+        }
 
-            
+    </style>
+</head>
+<body>
+    <div class="legend">
+        <strong>Legend</strong><br><br><br>
+        <span class="Basket"></span> Basket<br><br>
+        <span class="Gym"></span> Gym<br><br>
+        <span class="Personal_growth"></span> Personal<br>growth<br><br>
+        <span class="Sleep"></span> Sleep<br><br>
+        <span class="Other"></span> Other
+    </div>
 
-        <div class="legend">
-            <strong>Legend</strong><br><br><br>
-            <span class="Basket"></span> Basket<br><br>
-            <span class="Gym"></span> Gym<br><br>
-            <span class="Personal_growth"></span> Personal<br>growth<br><br>
-            <span class="Sleep"></span> Sleep
-        </div>
+    <?php
 
-        <?php
+        // PER OGNI GIORNO DELLA SETTIMANA, FARE UNA NUOVA PAGINA DOVE SI VEDA CHIARAMENTE LA DESCRIZIONE DEGLI ALLENAMENTI E 
+        // LE STESSE INFORMAZIONI DIViSE SEMPRE PER COLORE DI OGNI ATTIVITA' AD OGNI ORARIO
+        // (SE STESSA COSA RIPETUTA SU PIU' ORARI CONSECUTIVI, RAGGRUPPARE IN UNA CELLA SOLA (CON COLSPAN = NUMERO DI ORARI CONSECUTIVI IDENTICI))
 
-            // PER OGNI GIORNO DELLA SETTIMANA, FARE UNA NUOVA PAGINA DOVE SI VEDA CHIARAMENTE LA DESCRIZIONE DEGLI ALLENAMENTI E 
-            // LE STESSE INFORMAZIONI DIViSE SEMPRE PER COLORE DI OGNI ATTIVITA' AD OGNI ORARIO
-            // (SE STESSA COSA RIPETUTA SU PIU' ORARI CONSECUTIVI, RAGGRUPPARE IN UNA CELLA SOLA (CON COLSPAN = NUMERO DI ORARI CONSECUTIVI IDENTICI))
 
-            $orari_settimanali = array(
-                "Monday" => array("0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"),
-                "Tuesday" => array("0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"),
-                "Wednesday" => array("0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"),
-                "Thursday" => array("0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"),
-                "Friday" => array("0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"),
-                "Saturday" => array("0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"),
-                "Sunday" => array("0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00")
-            );
+        $orari_settimanali = array(
+            "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+        );
+        $orari = range(0, 23);
 
-            echo "<table>";
-            echo "<tr>
-                    <th></th>
-                    <th><a href='Train.php'>Monday</a></th>
-                    <th><a href='Train.php'>Tuesday</a></th>
-                    <th><a href='Train.php'>Wednesday</a></th>
-                    <th><a href='Train.php'>Thursday</a></th>
-                    <th><a href='Train.php'>Friday</a></th>
-                    <th><a href='Train.php'>Saturday</a></th>
-                    <th><a href='Train.php'>Sunday</a></th>
-                 </tr>";
+        echo "<table>";
+        echo "<tr><th></th>";
+        foreach ($orari_settimanali as $giorno) {
+            echo "<th><a href='Train.php'>$giorno</a></th>";
+        }
+        echo "</tr>";
 
-            for ($ora = 0; $ora <= 23; $ora++) {
-                $ora_display = sprintf("%02d:00", $ora);
-                echo "<tr>";
-                echo "<td>$ora_display</td>";
-                foreach ($orari_settimanali as $giorno => $orari) {
-                    /*if (in_array($ora_display, $orari)) {
-                        echo "<td class='Rest'></td>";
-                    } else {
-                        echo "<td></td>";
-                    }*/
-                    echo "<td></td>";
+        // METTI I MEZZI ORARI
+
+        foreach ($orari as $ora) {
+            $ora_display = sprintf("%02d:00", $ora);
+            echo "<tr><td>$ora_display</td>";
+            foreach ($orari_settimanali as $giorno) {
+                echo "<td class='";
+                if (($ora >= 0 && $ora <= 6) || $ora == 23) {
+                    echo "Sleep";
+                } elseif ($ora == 7) {
+                    echo "Other";
+                } elseif ($ora >= 11 && $ora <= 18) {
+                    echo "Basket";
+                } elseif ($ora >= 19 && $ora <= 22) {
+                    echo "Personal_growth";
                 }
-                
-                echo "</tr>";
+                echo "'></td>";
             }
-
-            echo "</table>";
-        ?>
-
-    </body>
+            echo "</tr>";
+        }
+        echo "</table>";
+    ?>
+</body>
 </html>
