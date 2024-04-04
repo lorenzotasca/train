5d51835959b15881361298132ae345533a79de60
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-04-04 09:29:17 +0000

    modified:   API/Delete/API.php 	modified:   API/Delete/APIDelete.php

--- API/Delete/API.php
+++ API/Delete/API.php
@@ -2,24 +2,6 @@
 
     include "../../connection/connection.php";
 
-    /*//read post param
-    $param = $_GET["t"];
-
-
-    //query
-    $sql = "SELECT * FROM Giocatore WHERE nome LIKE '%$param%'";
-
-    //execute query
-    $result = $connessione->query($sql);
-    
-
-    $res = $result->fetch_all();
-    foreach ($res as $r) {
-        echo '<input type="submit" class="delete-button" data-id="' . $r[0] . '" value="Delete"/>' . " ";
-        echo $r[2] . " " . $r[1] . "<br>";
-    }*/
-
-
 
     if(isset($_GET["t"])) {
         $param = $_GET["t"];
--- API/Delete/APIDelete.php
+++ API/Delete/APIDelete.php
@@ -3,37 +3,7 @@
     echo "Ciao";
 
     include "../../connection/connection.php";
-
-    /*// Ottenere l'ID dell'utente da eliminare dalla richiesta POST
-    $userId = $_POST["id"];
-    // Decodifica dell'ID ricevuto dalla richiesta POST
-    $decodedUserId = urldecode($userId);
-    //var_dump($_POST["id"]);
-    //var_dump($_POST); // Debug: stampa i dati POST per verificare la ricezione dell'ID
-    //$userId = $_GET['id'];
-
-    // Create a prepared SQL query to delete the user from the 'Giocatore' table with the specified ID
-    $sql = "DELETE FROM Giocatore WHERE ID_giocatore = ?";
-    $stmt = $connesione->prepare($sql);
-    $stmt->bind_param("s", $userId);
-    $stmt->execute();
-
-    // Check if the deletion was successful
-    if ($stmt->affected_rows > 0) {
-        // If the deletion was successful, print a confirmation message
-        echo "User deleted successfully";
-    } else {
-        // If there was an error during deletion, print an error message
-        echo "Error deleting user: " . $stmt->error;
-    }
-
-    $stmt->close();
-
-    // Chiudere la connessione al database
-    $conn->close();*/
-
-
-
+    
 
     if(isset($_POST["id"])) {
         // Prende l'ID utente dalla richiesta POST
@@ -41,6 +11,10 @@
 
         // Decodifica dell'ID ricevuto dalla richiesta POST
         $decodedUserId = urldecode($userId);
+
+        //var_dump($_POST["id"]);
+        //var_dump($_POST); // Debug: stampa i dati POST per verificare la ricezione dell'ID
+        //$userId = $_GET['id'];
 
         // Query SQL per eliminare l'utente dalla tabella 'Giocatore' con l'ID specificato
         $sql = "DELETE FROM Giocatore WHERE ID_giocatore = ?";
