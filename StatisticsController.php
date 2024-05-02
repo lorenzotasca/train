110c0b79a56bd81dc20f94b0e7c87f0c75585316
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2024-05-02 09:43:40 +0000

    modified:   StatisticsController.php

--- StatisticsController.php
+++ StatisticsController.php
@@ -23,7 +23,7 @@
         $stmt->bind_param("sss", $code, $_POST["shoot-type"], $quanto);
 
 
-        $query = "INSERT INTO registra (data) VALUES (?)";
+        $query = "INSERT INTO registra (data) VALUES (?)"; // METTI ANCHE LE CHIAVI ESTERNE E METTERE QUI UPDATE(NO INSERT, PERCHè L'INSERT IN QUESTA TABELLA, LO FARò IN REGISTRACONTROLLER.PHP)
         $stmt2 = $connessione->prepare($query);
         $stmt2->bind_param("s", $data);
 
@@ -36,10 +36,10 @@
     try {
         $stmt->execute();
         $stmt2->execute();
-        header("Location: Profile.php"); // vai in DefaultPage.php per poi avere in alto a destra un "menu a tendina" che ti dici molte cose tra cui: "my activities"
+        header("Location: Statistics.php");
     } catch (Exception $e) {
         $err = $e->getMessage();
-        header("Location: Characteristics.php?err=$err");
+        header("Location: Statistics.php?err=$err");
     } finally {
         $stmt->close();
         $stmt2->close();
